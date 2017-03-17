18792db94391ac648a929df877702151cb79e610
Eduardo Braga <ebfjunior@gmail.com>
2017-03-16 21:59:41 -0300

    @ebragaparah first commit, open house

---
+++ README.md
@@ -0,0 +1,6 @@
+# Ninety nine problems, solved in Elm
+
+## Problems
+
+1. Write a function last that returns the last element of a list.
+   An empty list doesn't have a last element, therefore last must return a Maybe.
---
+++ problem_1/Main.elm
@@ -0,0 +1,31 @@
+{- Write a function last that returns the last element of a list.
+   An empty list doesn't have a last element, therefore last must return a Maybe.
+-}
+
+
+module Main exposing (..)
+
+import Html exposing (text)
+import Maybe
+
+
+last : List a -> Maybe a
+last list =
+    case list of
+        [] ->
+            Nothing
+
+        [ x ] ->
+            Just x
+
+        x :: xs ->
+            last xs
+
+
+main =
+    case last [ 1, 2, 3, 4 ] of
+        Just a ->
+            text (toString a)
+
+        Nothing ->
+            text "No element found"
